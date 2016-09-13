82ffb773ad348e5b401a81ac513a828b5107e570
ZoeYuhan <suling_suling1989@163.com>
2016-09-13 23:02:22 +0800

    Update Logistic.py

--- algorithm/Logistic.py
+++ algorithm/Logistic.py
@@ -1,1 +1,10 @@
+import csv
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+
+iris = load_iris()
 
